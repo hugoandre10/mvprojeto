2fccc97c46b83ee7faef401a6a78a734c90416b8
hugoandre10 <hugog49@gmail.com>
2022-04-07 17:59:13 -0300

    deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/MvProjetoFront2'));
+app.use(express.static('./dist/MvProjetoFront'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/MvProjetoFront2/'}),
+    res.sendFile('index.html', {root: 'dist/MvProjetoFront/'}),
 );
 
 // Start the app by listening on the default Heroku port
