bfa2250f226840f7e672ea5ccbb5987fc1731db7
hugoandre10 <hugog49@gmail.com>
2022-04-07 18:45:03 -0300

    deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+{path:"" , redirectTo: "/home" , pathMatch:"full"},
 {path:"home" ,component:HomeComponent},
 {path:"listaFuncionarios",component:ListaFuncionarioComponent},
 {path:"cadastroFuncionarios",component:CadastrarFuncionarioComponent},
